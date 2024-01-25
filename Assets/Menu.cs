737480b95b8784bb043b0271018bf60bed8c11dc
amy1205 <153157040+amy1205@users.noreply.github.com>
2024-01-25 15:43:20 +0800

    2024/01/25音量控制

--- Assets/Menu.cs
+++ Assets/Menu.cs
@@ -1,18 +1,23 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
-
+using UnityEngine.UI;
+//使用Unity UI程式庫
 public class Menu : MonoBehaviour
 {
     [Header("首頁")]
     public GameObject MenuPage;
     [Header("設定")]
     public GameObject SettingPage;
-    
+    //----------------------------------------------------
+    [Header("控制音量的Slider")]
+    public Slider ControlVolumSlider;
+    [Header("AudioListener")]
+    public AudioListener AudioListenerObject;
     // Start is called before the first frame update
     void Start()
     {
-        
+        AudioListener.volume= ControlVolumSlider.value;
     }
 
     // Update is called once per frame
@@ -31,5 +36,20 @@ public class Menu : MonoBehaviour
         //關閉遊戲
         Application.Quit();
 
+    }
+    public void SetSetting(bool Set)
+    {
+        //控制首頁頁面開關
+        MenuPage.SetActive(Set);
+        //控制設定頁面開關
+        SettingPage.SetActive(!Set);
+
+    }
+    public void SetMusicVolum()
+    {
+        //整體聲音的音量=聲音控制Slider的數值(值介於0~1之間)
+        AudioListener.volume= ControlVolumSlider.value;
+        //將聲音的音量儲存在SaveData腳本中的SaveVolume變數內
+        SaveData.SaveVolume = AudioListener.volume;
     }
 }
---
+++ Assets/SaveData.cs
@@ -0,0 +1,9 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SaveData : MonoBehaviour
+{
+    //儲存整體遊戲的音量
+     public static float SaveVolume;
+}
